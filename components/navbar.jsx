246fa8868c0a46fe108e8df8a2e05b6921ac7817
maxraulea <max.raulea@gmail.com>
2022-12-28 20:10:37 +0100

    tailwind

---
+++ components/navbar.jsx
@@ -0,0 +1,16 @@
+const navbar = ({heading ,message}) =>{
+    return(   
+        <div id="vision" className="w-full h-screen text-center ">
+            <div className="max-w-[1240px] w-full h-full mx-auto flex justify-center items-center">
+                <div>
+                    <h1>Lorem Ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet. Qui eligendi galisum et dolore nisi aut officiis minus ut molestiae iusto. Hic veniam voluptate aut laborum ducimus ad suscipit pariatur. Sit aspernatur reprehenderit hic quia perferendis sit iste magnam. Ut perferendis galisum eum praesentium optio et molestias iusto in eius fugiat et corporis amet ad praesentium nihil? Id ipsam deleniti aut corporis explicabo et necessitatibus tempore aut tempora dolorem.</p>
+                    <br />
+                    <p>Nam possimus eius qui voluptatum iure ut galisum dolor. Eum voluptatem omnis et eligendi consequuntur qui architecto iusto rem cupiditate molestiae aut libero modi. Aut adipisci dolorem qui nihil dolor eos corrupti asperiores aut deleniti rerum qui accusantium quod aut fuga obcaecati?</p>
+                </div>
+            </div>
+        </div>
+     )
+}
+
+export default navbar
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,13 +11,9 @@ export default function Home() {
     <>
       <div>
         <Head>
-          <title>yo whats up</title>
+          <title>Max Răulea</title>
         </Head>
-        <div className="text-green">
-          <h1 className="text-white font-bold">
-            faka
-          </h1>
-        </div>
+        <Navbar/>
       </div>
     </>
   )
